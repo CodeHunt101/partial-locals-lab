90fa33e1cae3c9719e7b41bf4e40cdc99d7b9d14
Harold Torres Marino <haroldtm55@gmail.com>
2021-10-17 19:55:38 +1100

    commit tree 98a41f094f75806f9c220bfe60ce847b6f422060

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,12 +16,18 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def update
+    @student = Student.find(params[:id])
+    @student.update(student_params)
+    redirect_to student_path(@student)
+  end
+
   def show
     @student = Student.find(params[:id])
   end
 
   def index
-    @students = Student.all
+    @students = Student.search(params[:query])
   end
 
   def student_params
